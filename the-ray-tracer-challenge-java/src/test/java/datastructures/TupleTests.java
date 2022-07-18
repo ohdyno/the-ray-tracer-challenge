4bf3c08de05a194fbb0824117e488d3a42cbd798
Xing Zhou <xing@xingzhou.me>
2022-07-17 22:39:43 -0400

    refactor: group tuple tests

---
+++ the-ray-tracer-challenge-java/src/test/java/datastructures/TupleTests.java
@@ -0,0 +1,43 @@
+package datastructures;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TupleTests {
+
+    @Nested
+    @DisplayName("A tuple exhibits behaviors like a value.")
+    class ValueBehaviorTests {
+        @Test
+        @DisplayName("A tuple with w=1.0 is a point.")
+        void aTupleThatIsAPoint() {
+            final Tuple tuple = Tuple.point(4.3, -4.2, 3.1);
+            assertEquals(4.3, tuple.getX(), 0.00001);
+            assertEquals(-4.2, tuple.getY(), 0.00001);
+            assertEquals(3.1, tuple.getZ(), 0.00001);
+            assertTrue(tuple.isAPoint());
+            assertFalse(tuple.isAVector());
+        }
+
+        @Test
+        @DisplayName("A tuple with w=0.0 is a vector.")
+        void aTupleThatIsAVector() {
+            final Tuple tuple = Tuple.vector(4.3, -4.2, 3.1);
+            assertEquals(4.3, tuple.getX(), 0.00001);
+            assertEquals(-4.2, tuple.getY(), 0.00001);
+            assertEquals(3.1, tuple.getZ(), 0.00001);
+            assertTrue(tuple.isAVector());
+            assertFalse(tuple.isAPoint());
+        }
+
+        @Test
+        @DisplayName("Two tuples are equal if they are the same type and their coordinates are the same")
+        void equality() {
+            assertNotEquals(Tuple.vector(4.3, -4.2, 3.1), Tuple.point(4.3, -4.2, 3.1));
+            assertEquals(Tuple.vector(4.3, -4.2, 3.1), Tuple.vector(4.3, -4.2, 3.1));
+        }
+    }
+}
